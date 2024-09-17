79fe8f22ba5d2da4e3f31c208f647b3a2f27e3c2
Midget807 <phu7mai@gmail.com>
2024-09-17 10:30:40 +0930

    Initial commit

---
+++ src/client/java/net/midget807/trapsntrickery/TrapsAndTrickeryClient.java
@@ -0,0 +1,10 @@
+package net.midget807.trapsntrickery;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrapsAndTrickeryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/midget807/trapsntrickery/TrapsAndTrickeryDataGenerator.java
@@ -0,0 +1,11 @@
+package net.midget807.trapsntrickery;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TrapsAndTrickeryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
